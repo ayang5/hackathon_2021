4e9d88d0a55d5ccee58626e6a944b7781e5aaf61
BigDjb101587 <96355546+BigDjb101587@users.noreply.github.com>
2021-12-18 14:20:14 -0700

    Add files via upload

---
+++ algorithm.js
@@ -0,0 +1,18 @@
+function Protein1(choice,serving){
+    console.log("hey");
+    if(choice == "Chicken"){
+        return 300*serving;
+    }
+} 
+function Dairy1(){
+
+}
+function Veggies1(){
+
+}
+function Fruits1(){
+
+}
+function Grains1(){
+
+}
